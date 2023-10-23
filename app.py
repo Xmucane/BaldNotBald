7d4038d32609361df3d693db8fe5e771ff492af4
Burak <54419081+Xmucane@users.noreply.github.com>
2023-10-23 16:56:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.models import load_model
+from keras.preprocessing import image
+from keras.models import load_model
 from werkzeug.utils import secure_filename
 import os
 import numpy as np
-from PIL import Image
 
 app = Flask(__name__)
+# Yüklenen dosyaların saklandığı klasörün adı
 app.config['UPLOAD_FOLDER'] = 'Test'
 model = load_model('modelim.h5', compile=True)
 
@@ -16,34 +16,29 @@ def uploaded_file(filename):
 
 @app.route('/', methods=['GET', 'POST'])
 def classify_image():
+    predicted_class = None
+    uploaded_filename = None
+
     if request.method == 'POST':
+        # Gelen dosyayı işle
         uploaded_file = request.files['file']
         if uploaded_file.filename != '':
+            # Dosyayı güvenli bir şekilde kaydet
             filename = secure_filename(uploaded_file.filename)
-            uploaded_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            uploaded_file.save(os.path.join(
+                app.config['UPLOAD_FOLDER'], filename))
 
-            # İşlem sonrası dosyaları kapat
-            with Image.open(os.path.join(app.config['UPLOAD_FOLDER'], filename)) as img:
-                img = img.resize((64, 64))
-                img = image.img_to_array(img)
-                img = np.expand_dims(img, axis=0)
+            # Kaydedilen dosyayı işle
+            img = image.load_img(os.path.join(
+                app.config['UPLOAD_FOLDER'], filename), target_size=(64, 64))
+            img = image.img_to_array(img)
+            img = np.expand_dims(img, axis=0)
 
-                predictions = model.predict(img)
-                predicted_class = int(predictions[0][0])
+            predictions = model.predict(img)
+            predicted_class = int(predictions[0][0])
+            uploaded_filename = filename
 
-                return render_template('result.html', predicted_class=predicted_class, uploaded_filename=filename)
-
-    return render_template('index.html')
+    return render_template('index.html', predicted_class=predicted_class, uploaded_filename=uploaded_filename)
 
 if __name__ == '__main__':
-    from gunicorn.app.wsgiapp import WSGIApplication
-
-    gunicorn_opts = {
-        'bind': '0.0.0.0:10000',  # Gunicorn'un çalıştığı adres ve port
-        'workers': 2,  # İşçi sayısı
-        'worker_class': 'gevent',  # İşçi sınıfı
-        'worker_memory_limit': "200M",  # Bellek sınırlamasını azaltın
-        'timeout': 60  # Zaman aşımı
-    }
-
-    WSGIApplication("%(prog)s [OPTIONS]").run()
+    app.run(debug=False)
