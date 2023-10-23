cf7bbbb82da5a232a4b438d38b5a72505dfe1e6b
Burak <54419081+Xmucane@users.noreply.github.com>
2023-10-23 21:24:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 from keras.preprocessing import image
 from keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import load_model
 from werkzeug.utils import secure_filename
 import os
 import numpy as np
