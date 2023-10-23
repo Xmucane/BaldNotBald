2ed6b5aec3098ef45512d384a44976754b88c4bb
Burak <54419081+Xmucane@users.noreply.github.com>
2023-10-23 14:59:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
-from tensorflow.keras.preprocessing import image  # TensorFlow Keras kullanıyoruz
-from tensorflow.keras.models import load_model  # TensorFlow Keras kullanıyoruz
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import load_model
 from werkzeug.utils import secure_filename
 import os
 import numpy as np
-from PIL import Image  # Pillow'ı içe aktar
+from PIL import Image
 
 app = Flask(__name__)
-# Yüklenen dosyaların saklandığı klasörün adı
 app.config['UPLOAD_FOLDER'] = 'Test'
 model = load_model('modelim.h5', compile=True)
 
@@ -18,23 +17,21 @@ def uploaded_file(filename):
 @app.route('/', methods=['GET', 'POST'])
 def classify_image():
     if request.method == 'POST':
-        # Gelen dosyayı işle
         uploaded_file = request.files['file']
         if uploaded_file.filename != '':
-            # Dosyayı güvenli bir şekilde kaydet
             filename = secure_filename(uploaded_file.filename)
             uploaded_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-            # Kaydedilen dosyayı işle
-            img = Image.open(os.path.join(app.config['UPLOAD_FOLDER'], filename))  # Pillow ile aç
-            img = img.resize((64, 64))
-            img = image.img_to_array(img)
-            img = np.expand_dims(img, axis=0)
+            # İşlem sonrası dosyaları kapat
+            with Image.open(os.path.join(app.config['UPLOAD_FOLDER'], filename)) as img:
+                img = img.resize((64, 64))
+                img = image.img_to_array(img)
+                img = np.expand_dims(img, axis=0)
 
-            predictions = model.predict(img)
-            predicted_class = int(predictions[0][0])
+                predictions = model.predict(img)
+                predicted_class = int(predictions[0][0])
 
-            return render_template('result.html', predicted_class=predicted_class, uploaded_filename=filename)
+                return render_template('result.html', predicted_class=predicted_class, uploaded_filename=filename)
 
     return render_template('index.html')
 
